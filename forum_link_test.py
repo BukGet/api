2ead6268ca07644b5842103e8dd7a05e98c3c328
Steven McGrath <steven.mcgrath@us.thewg.com>
2011-04-29 11:17:15 -0500

    Bukkit Forums Integration Testing

---
+++ forum_link_test.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+ 
+import httplib, urllib
+
+class BukkitForum(object):
+  host      = 'forums.bukkit.org'
+  username  = None
+  password  = None
+  cookies   = {}
+  headers   = {
+   'Cache-Control': 'max-age=0',
+ 'Accept-Language': 'en-US,en;q=0.8',
+  'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+          'Accept': 'application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; en-US) '+\
+                    'AppleWebKit/534.16 (KHTML, like Gecko) '+\
+                    'Chrome/10.0.648.205 Safari/534.16',
+  }
+  
+  def __init__(self, username, password):
+    self.username = username
+    self.password = password
+  
+  def __update_cookies(self, cookies):
+    print cookies
+    if cookies is not None:
+      for cookie in cookies.split(';'):
+        dset        = cookie.split('=')
+        if len(dset) > 1:
+          if dset[0] in ['f_user', 'f_session', 'IDstack']:
+            self.cookies[dset[0]] = dset[1]
+  
+  def __logged_in(self, page):
+    if page[:100].find('class="Public LoggedOut"') > -1:
+      return False
+    elif page[:100].find('class="Public LoggedIn"') > -1:
+      return True
+    else:
+      return 'UNKNOWN'
+  
+  def __get_cookies(self):
+    cookies = []
+    for cookie in self.cookies:
+      cookies.append('%s=%s; ' % (cookie, self.cookies[cookie]))
+    print '; '.join(cookies)
+    return '; '.join(cookies)
+  
+  def __get(self, loc):
+    con           = httplib.HTTPConnection(self.host)
+    cookies       = self.__get_cookies()
+    headers       = self.headers
+    if cookies is not '':
+      headers['Cookie'] = cookies
+    con.request('GET', loc, headers=headers)
+    resp          = con.getresponse()
+    print resp.getheaders()
+    self.__update_cookies(resp.getheader('set-cookie'))
+    newloc        = resp.getheader('location')
+    if newloc is not None:
+      print 'Chasing Referral: %s' % newloc
+      page        = self.__get(newloc)
+    else:
+      page        = resp.read()
+    return page
+  
+  def __post(self, loc, formdata):
+    con           = httplib.HTTPConnection(self.host)
+    cookies       = self.__get_cookies()
+    headers       = self.headers
+    payload       = urllib.urlencode(formdata)
+    if cookies is not '':
+      headers['Cookie']       = cookies
+    headers['Host']           = 'forums.bukkit.org'
+    headers['Origin']         = 'http://forums.bukkit.org/'
+    headers['Referer']        = 'http://forums.bukkit.org/'
+    headers['Content-Type']   = 'application/x-www-form-urlencoded'
+    headers['Content-Length'] = len(payload)
+    print headers
+    con.request('POST', loc, headers=headers, body=payload)
+    resp          = con.getresponse()
+    print resp.getheaders()
+    self.__update_cookies(resp.getheader('set-cookie'))
+    newloc        = resp.getheader('location')
+    if newloc is not None:
+      print 'Chasing Referral: %s' % newloc
+      page        = self.__get(newloc)
+    else:
+      page        = resp.read()
+    return page
+  
+  def login(self):
+    #self.__get('/')
+    formdata  = {
+         'login': self.username,
+      'register': 0,
+      'password': self.password,
+      'remember': 0,
+  'cookie_check': 1,
+      'redirect': '/',
+      '_xfToken': '',
+    }
+    page = self.__post('/login', formdata)
+    return self.__logged_in(page)
+  
+  def private_message(self, user, message, locked=False):
+    pass
+    
+    
+
+if __name__ == '__main__':
+  forum   = BukkitForum('XXX', 'XXX')
+  print forum.login()
+  print forum.cookies
