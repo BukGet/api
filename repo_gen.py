a42df7a673a9b4a77cf7015cb966e6edb41bc486
Steven McGrath <steven.mcgrath@us.thewg.com>
2011-05-10 15:48:21 -0500

    Added Account Activation code.

--- repo_gen.py
+++ repo_gen.py
@@ -12,6 +12,8 @@ import getopt
 import urllib2
 import json
 import re
+import hashlib
+from pyxenforo                  import XenForo
 from BeautifulSoup              import BeautifulSoup    as bsoup
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy                 import Table, Column, Integer, String, \
@@ -19,6 +21,16 @@ from sqlalchemy                 import Table, Column, Integer, String, \
                                        Boolean, create_engine, MetaData, and_
 from sqlalchemy.orm             import relation, backref, sessionmaker
 
+
+def _gen_act_msg(plugin, code):
+  return '''
+<p>Dear Plugin Developer</p>
+<p>Your account has recently been tagged with a new plugin repository on our
+server.  In order to validate that you really did request a new repository
+antry for %s, we are asking you to click the link below to validate
+yourself so that we may activate this entry in our records.</p>
+<p><a href="http://bukget.org/activate.php?hash=%s">ACTIVATION LINK</a></p>
+''' % (plugin, code)
 
 def _config(stanza, option, opt_type='string'):
   '''
@@ -57,8 +69,8 @@ class RepoLink(Base):
   email         = Column(String(128))
   url           = Column(Text)
   plugin        = Column(String(32))
+  hash          = Column(String(32))
   activated     = Column(Boolean)
-  validated     = Column(Boolean)
   ip_address    = Column(String(15))
   created       = Column(DateTime)
   
@@ -66,19 +78,33 @@ class RepoLink(Base):
     self.username = username
     self.url      = url
   
-  def activate(self):
-    #plu_dict      = json.loads(\
-    #                  urllib2.urlopen(\
-    #                    'http://plugins.bukkit.org/data.php').read())
-    #name          = re.compile(r'^(?:\[.+?\]){0,1}\s(\w+[^ ])')
-    #for item in plu_dict:
-    #  plugin      = name.findall(item['title'])[0]
-    #  if plugin.lower() == self.plugin.lower():
-    #    if item['author'].lower() == self.author.lower():
-    #      self.activated  = True
-    #      return True
+  def send_activation(self):
+    try:
+      plugins     = json.loads(\
+                      urllib2.urlopen(\
+                        'http://plugins.bukkit.org/data.php').read())
+    except:
+      return False
+    name_rex      = re.compile(r'^(?:\[.+?\]){0,1}\s(\w+[^ ])')
+    for plugin in plugins:
+      name        = name_rex.findall(plugin['title'])[0]
+      if name is not None:
+        if name.lower() == self.plugin.lower():
+          if plugin['author'].lower() == self.author.lower():
+            forum = XenForo(_config('Forum', 'username'),
+                            _config('Forum', 'password'),
+                            _config('Forum', 'hostname'))
+            if forum.login():
+              md5 = hashlib.md5()
+              md5.update(self.name + self.author +\
+                         datetime.datetime.now().ctime())
+              self.hash = md5.hexdigest()
+              forum.private_message(self.author,
+                                    'BukGet Activation Code',
+                                    _gen_act_msg(self.plugin, self.hash), 
+                                    locked=True)
+              return True
     return False
-    
   
   def fetch(self):
     if self.activated:
@@ -129,15 +155,14 @@ def generate_repository():
   repo    = []
   logfile = open(_config('Settings', 'log_file'), 'a')
   for link in links:
-    if not link.activated:
-      link.activate()
-    if link.fetch():
-      repo.append(link.data)
-      logfile.write('%s: %s added to canonical repository.' %\
-        (datetime.datetime.now().ctime(), link.plugin))
-    else:
-      print '%s: %s failed with status: %s' %\
-       (datetime.datetime.now().ctime(), link.plugin, link.status)
+    if link.activated:
+      if link.fetch():
+        repo.append(link.data)
+        logfile.write('%s: %s added to canonical repository.' %\
+          (datetime.datetime.now().ctime(), link.plugin))
+      else:
+        print '%s: %s failed with status: %s' %\
+         (datetime.datetime.now().ctime(), link.plugin, link.status)
   rfile   = open(_config('Settings', 'repo_file'), 'w')
   logfile.write('%s: Writing out new canonical repository file.' %\
     datetime.datetime.now().ctime())
@@ -146,7 +171,21 @@ def generate_repository():
   logfile.write('%s: Complete.' % datetime.datetime.now().ctime())
   logfile.close()
 
+def run_activations():
+  session = Session()
+  links   = session.query(RepoLink)
+  for link in links:
+    if not link.activated:
+      if link.hash is None:
+        link.send_activation()
+
 if __name__ == '__main__':
-  sys.exit(generate_repository())
+  if len(sys.argv) > 1:
+    if sys.argv[1] == 'repository':
+      sys.exit(generate_repository())
+    if sys.argv[1] == 'activate':
+      sys.exit(run_activations())
+  print 'repository = Runs repository Generation.\n'+\
+        'activate = Runs link activations.'
   
   
