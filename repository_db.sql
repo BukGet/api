42975f544a415634aca5497899937e80b71d7bdb
Steven McGrath <steven.mcgrath@us.thewg.com>
2011-05-11 14:36:17 -0500

    Updated DB Schema.

--- repository_db.sql
+++ repository_db.sql
@@ -5,8 +5,7 @@ CREATE TABLE repositories (
   email       CHAR(128)           NOT NULL,
   url         TEXT                NOT NULL,
   plugin      CHAR(32)            NOT NULL UNIQUE,
-  ip_address  CHAR(15)            NOT NULL,
   created     DATETIME            NOT NULL,
   activated   BOOLEAN             NOT NULL DEFAULT 0,
-  validated   BOOLEAN             NOT NULL DEFAULT 0
+  hash        CHAR(32)            NOT NULL
 );
