e88eef3b547d39a7c9dfd78f482daa82dd48a641
Steven McGrath <steve@chigeek.com>
2011-06-04 00:47:04 -0500

    Cleaning up the repository

--- bukget/__init__.py
+++
@@ -1,5 +0,0 @@
-import pkg
-import client
-import server
-
-version = '0.0.1-git'
--- bukget/logging.py
+++
@@ -1,7 +0,0 @@
-import config
-import datetime
-
-def log(msg):
-  logfile = open(config.get('Paths', 'logfile'), 'a')
-  logfile.write('%s: %s\n' % (datetime.datetime.now().ctime(), msg)
-  logfile.close()
