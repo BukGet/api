4f66ac3b328dd2b3eca42b4952c4ca53cf84edea
Steven McGrath <steven.mcgrath@us.thewg.com>
2011-06-06 14:07:32 -0500

    Fixed validation and repo.json generation.

--- server/app.py
+++ server/app.py
@@ -181,82 +181,38 @@ class Repository(Base):
     # of the definitions that should exist within the json dictionary.  If
     # there are any missing items or malformed items, we should be returning
     # false.  If everything does check out, then True should be returned.
-    
-    # This block checks to see if name exists and that it is a string.
     if 'name' not in d: return False
-    if not isinstance(d['name'], str): return False
-    
-    # This block checks to see if authors exists and that it is a list.
+    if not isinstance(d['name'], unicode): return False
     if 'authors' not in d: return False
     if not isinstance(d['authors'], list): return False
-    
-    # This block checks to see if maintainer exists and that it is a string.
     if 'maintainer' not in d: return False
-    if not isinstance(d['maintainer'], str): return False
-    
-    # Now lets check to see that decription is here and that it is a string
+    if not isinstance(d['maintainer'], unicode): return False
     if 'description' not in d: return False
-    if not isinstance(d['description'], str): return False
-    
-    # Checking to see if website exists and that it is a string.
+    if not isinstance(d['description'], unicode): return False
     if 'website' not in d: return False
-    if not isinstance(d['website'], str): return False
-    
-    # Checking to make sure that categories  exists and that it is a list.
+    if not isinstance(d['website'], unicode): return False
     if 'categories' not in d: return False
     if not isinstance(d['categories'], list): return False
-    
-    # and for our last basic check, lets make sure versions exists and that
-    # it is also a list object.  Validation for the individual versions will
-    # be handled by the child class.
     if 'versions' not in d: return False
     if not isinstance(d['versions'], list): return False
-    
-    # Now we need to iterate through all the versions and check them to make 
-    # sure that they match just like with the base information.
     for v in d['versions']:
-      # To start we shoudl check to see that the version definition exists and
-      # that it is a string type.
       if 'version' not in v: return False
-      if not isinstance(v['version'], str): return False
-      
-      # Moving on to required_dependencies.  This should also be a list.
-      if 'required_dependencies' not in d: return False
-      if not isinstance(d['required_dependencies'], list): return False
-
-      # And now for optional_dependencies.  Again should be a list.
-      if 'optional_dependencies' not in d: return False
-      if not isinstance(d['optional_dependencies'], list): return False
-      
-      # Next up is conflictions.  This should be a list as well.
-      if 'conflicts' not in d: return False
-      if not isinstance(d['conflicts'], list): return False
-      
-      # Next up is the location definition.  There should really be some extra
-      # checking here eventually to make sure this is a URL as well.
+      if not isinstance(v['version'], unicode): return False
+      if 'required_dependencies' not in v: return False
+      if not isinstance(v['required_dependencies'], list): return False
+      if 'optional_dependencies' not in v: return False
+      if not isinstance(v['optional_dependencies'], list): return False
+      if 'conflicts' not in v: return False
+      if not isinstance(v['conflicts'], list): return False
       if 'location' not in v: return False
-      if not isinstance(v['location'], str): return False
-
-      # And now for the checksum.  Same as the other two we just need to check
-      # to make sure it exists and that it's a string.
+      if not isinstance(v['location'], unicode): return False
       if 'checksum' not in v: return False
-      if not isinstance(v['checksum'], str): return False
-
-      # And now for the branch.  With branch we actually have 3 checks.  we 
-      # need to make sure it exists, is a string, and is one of the valid 
-      # options.
+      if not isinstance(v['checksum'], unicode): return False
       if 'branch' not in v: return False
-      if not isinstance(v['branch'], str): return False
+      if not isinstance(v['branch'], unicode): return False
       if v['branch'] not in ['stable','test','dev']: return False
-
-      # This is the bukkit build lower bounds check.  Needs to be an integer.
       if 'bukkit_min' not in v: return False
       if not isinstance(v['bukkit_min'], int): return False
-
-      # And the previous items counterpart, this is the bukkit build upper 
-      # bounds check.  As before it needs to be an integer.  One last added
-      # here is simple to make sure that max really is less than or equal to
-      # the minimum.
       if 'bukkit_max' not in v: return False
       if not isinstance(v['bukkit_max'], int): return False
       if not v['bukkit_min'] <= v['bukkit_max']: return False
@@ -275,14 +231,14 @@ class Repository(Base):
       log.error('Could not read from %s\'s package.json' % self.plugin)
       return False
     if not self._valid(d):
-      log.error('%s\'s remote package.json does not appear to be valid.')
+      log.error('%s\'s remote package.json does not appear to be valid.' % self.plugin)
       return False
     
     # New we are going to compare the newly downloaded data to the one we have
     # in the cache.  if the 2 don't match, then we will update the cache with
     # the newer information.
     cur = hashlib.md5()
-    cur.update(self.cache)
+    cur.update(str(self.cache))
     new = hashlib.md5()
     new.update(newd)
     if new.hexdigest() != cur.hexdigest():
@@ -310,9 +266,12 @@ class Repository(Base):
       return False
     for plugin in bktdata:
       name = rname.findall(plugin['title'])
-      if name == self.plugin:
-        if plugin['author'] == self.maintainer:
-          check_ok = True
+      if isinstance(name, list):
+        if name[0].lower() == self.plugin.lower():
+          if plugin['author'].lower() == self.maintainer.lower():
+            log.info('Found entry')
+            check_ok = True
+    log.info('check_ok is %s' % check_ok)
     if check_ok:
       forum = XenForo(config.get('Forum', 'username'), 
                       config.get('Forum', 'password'),
@@ -363,6 +322,7 @@ def add_repo():
         s.merge(repo)
         s.commit()
         notes.append('Activation Successful!')
+        log.info('%s is now  active and will be cached next generation cycle.' % name)
       else:
         errors.append('Activation Failed.  Activation Hash did not match.')
     
@@ -426,7 +386,7 @@ def github_redirect():
 
 @route('/repo.json')
 def get_repo_file():
-  return static_file('repo.json', config.get('Settings','repo_file'))
+  return static_file('repo.json', config.get('Settings','static_files'))
 
 @route('/help')
 def help_page():
@@ -445,9 +405,11 @@ def generate_repository():
       if repo.update():
         s.merge(repo)
         s.commit()
-      rdict.append(repo.cache)
-    rfile = open(config.get('Settings', 'repo_file'), 'w')
-    rfile.write(json.dumps('\n'.join(rdict)))
+      if repo.cache is not None:
+        rdict.append(json.loads(repo.cache))
+    rfile = open(os.path.join(\
+                 config.get('Settings', 'static_files'),'repo.json'), 'w')
+    rfile.write(json.dumps(rdict))
     rfile.close()
     return '{"plugins": %s}' % len(repos)
   return '{"error": "Not an allowed address"}'
