8a5dd479d87dbca0246a052ee59657bcfedfcd69
Steven McGrath <steven.mcgrath@us.thewg.com>
2011-06-09 11:58:53 -0500

    Fleshed out add_news.py.  working properly.

--- server/add_news.py
+++ server/add_news.py
@@ -4,7 +4,11 @@ from sqlalchemy import (Table, Column, Integer, String, DateTime, Date,
                         ForeignKey, Text, Boolean, create_engine, MetaData, 
                         and_, desc)
 from sqlalchemy.orm import relationship, backref, sessionmaker
+from ConfigParser import ConfigParser
+import datetime
 
+config = ConfigParser()
+config.read('config.ini')
 Base = declarative_base()
 
 class NewsArticle(Base):
@@ -29,12 +33,22 @@ class NewsArticle(Base):
     markdown = markdown2.Markdown()
     return markdown.convert(self.data)
 
+def getdate(s):
+  year, month, day = s.split()[0].split('-')
+  hour, minute = s.split()[1].split(':')
+  return datetime.datetime(int(year), int(month), int(day), 
+                           int(hour), int(minute))
+  
+
 engine = create_engine(config.get('Settings', 'db_string'))
 Session = sessionmaker(bind=engine)
 NewsArticle.metadata.create_all(engine)
 session = Session()
 article = open('article.md').read()
 title = article.split('\n')[0]
-post = '\n'.join(article.split('\n')[1:])
+date = getdate(article.split('\n')[1])
+post = '\n'.join(article.split('\n')[2:])
 news = NewsArticle(title, post)
 session.add(news)
+session.commit()
+session.close()
