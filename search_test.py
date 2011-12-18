83f37464dd107ec2cee406c75fb3acad7723c136
Steven McGrath <steve@chigeek.com>
2011-12-18 11:23:15 -0600

    Search testing script

---
+++ search_test.py
@@ -0,0 +1,28 @@
+import httplib
+from urllib import urlencode
+
+class Tester(object):
+    host = 'localhost'
+    port = 8082
+    
+    def check(self, fieldname, action, value):
+        http = httplib.HTTPConnection(self.host, port=self.port)
+        payload = urlencode({
+            'fieldname': fieldname,
+            'action': action,
+            'value': value
+        })
+        headers = {'Content-Length': len(payload),
+                   'Content-Type': 'application/x-www-form-urlencoded'}
+        http.request('POST', '/api/search', body=payload, headers=headers)
+        resp = http.getresponse()
+        print resp.read()
+
+if __name__ == '__main__':
+    bukget = Tester()
+    bukget.check('authors', 'in', 'LordNed')
+    #bukget.check('v_game_builds', 'in', '1337')
+    #bukget.check('name', 'like', 'essential')
+    #bukget.check('v_md5', '=', '2032eabffbe58378a3393e4ed881e462')
+    #bukget.check('v_date', '>=', '1314231157')
+    
