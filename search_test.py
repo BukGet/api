8f83b5ed67e43fe8d422236a6b7ea7178599aa07
Steven McGrath <steve@chigeek.com>
2011-12-20 09:56:04 -0600

    Minor adjustment to tester

--- search_test.py
+++ search_test.py
@@ -2,8 +2,8 @@ import httplib
 from urllib import urlencode
 
 class Tester(object):
-    host = 'localhost'
-    port = 8082
+    host = 'bukget.org'
+    port = 80
     
     def check(self, fieldname, action, value):
         http = httplib.HTTPConnection(self.host, port=self.port)
@@ -21,8 +21,8 @@ class Tester(object):
 if __name__ == '__main__':
     bukget = Tester()
     bukget.check('authors', 'in', 'LordNed')
-    #bukget.check('v_game_builds', 'in', '1337')
-    #bukget.check('name', 'like', 'essential')
-    #bukget.check('v_md5', '=', '2032eabffbe58378a3393e4ed881e462')
-    #bukget.check('v_date', '>=', '1314231157')
+    bukget.check('v_game_builds', 'in', '1337')
+    bukget.check('name', 'like', 'essential')
+    bukget.check('v_md5', '=', '2032eabffbe58378a3393e4ed881e462')
+    bukget.check('v_date', '>=', '1314231157')
     
