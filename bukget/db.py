59c9ccea0fd090088080ba1063dc98bd8f9aad16
Steven McGrath <smcgrath@tenable.com>
2012-06-12 21:43:12 -0500

    Made needed changed to ORM to make it functional

--- bukget/db.py
+++ bukget/db.py
@@ -1,31 +1,70 @@
 import json
-import bukget.config
+from bukget.config import config
 from sqlalchemy import (Table, Column, Integer, ForeignKey, PickleType, Text,
-						and_, desc, create_engine)
+                        String, DateTime, and_, desc, create_engine)
 from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
+class NewBase(object):
+    # This is a base class that we will be using for the Database.
+    # I am expanding upon the original declarative_base class and
+    # adding in the json function (and potentially others) here in
+    # order to conform to DRY principles ;)
+    def json(self, *fields):
+        '''json *fields
+        Returns a Python Dictionary element with the items specified
+        in the fields list.  If no items are specified, then return
+        the endire dictionary.
+        '''
+        jdict = {}
+        if len(fields) == 0:
+            for field in fields:
+                jdict[field] = self.__dict__[field]
+                # Some code will have to be added here to account for
+                # relationship models, however I will have to run some
+                # tests first.
+        else:
+            jdict = self.__dict__
+        return jdict
 
-DBase = declarative_base()
+
+
+Base = declarative_base(cls=NewBase)
 
 # This is the on-disk database
-disk = create_engine(bukget.conf.get('Settings', 'db_string'))
+disk = create_engine(config.get('Settings', 'db_string'))
 Session = sessionmaker(disk)
 
 # This is the in-memory database
-memory = create_engine('sqlite://:memory:')
+memory = create_engine('sqlite:///:memory:')
 Reactor = sessionmaker(memory)
 
 
+# The Category Association Table.  This table houses the relationships
+# between plugins and categories.
+catassc = Table('catagory_associations', Base.metadata,
+    Column('plugin_id', Integer, ForeignKey('plugin.id')),
+    Column('category_id', Integer, ForeignKey('category.id'))
+)
+
+
+# The Author Association Table.  This table houses the relationships
+# between authors and plugins.
+authassc = Table('author_association', Base.metadata,
+    Column('author_id', Integer, ForeignKey('author.id')),
+    Column('plugin_id', Integer, ForeignKey('plugin.id'))
+)
+
+
 def initialize(engine):
-	Plugin.metadata.create_all(engine)
-	Version.metadata.create_all(engine)
-	Repo.metadata.create_all(engine)
-	Category.metadata.create_all(engine)
-	Author.metadata.create_all(engine)
-	catassc.create(engine)
-	authassc.create(engine)
+    Plugin.metadata.create_all(engine)
+    Version.metadata.create_all(engine)
+    Repo.metadata.create_all(engine)
+    Category.metadata.create_all(engine)
+    Author.metadata.create_all(engine)
+    catassc.metadata.create_all(engine)
+    authassc.metadata.create_all(engine)
 
 
 def clear(engine):
@@ -48,12 +87,12 @@ def _migrate(s, d, table):
 
 
 def clone(source, dest):
-	'''clone source dest
-	This function will completely clone the Database from the source engine to
-	the destination engine.  This is very useful for replicating information
-	in and out of memory as we will be primarially using an in-memory sqlite
-	database for BukGet for performance purposes.
-	'''
+    '''clone source dest
+    This function will completely clone the Database from the source engine to
+    the destination engine.  This is very useful for replicating information
+    in and out of memory as we will be primarially using an in-memory sqlite
+    database for BukGet for performance purposes.
+    '''
     # First we need to scrub and initialize the destination to make sure that it
     # is primed for us to work our magic ;)
     clear(dest)
@@ -91,105 +130,89 @@ def replace(engine):
     in-memory copy and then once everything is complete a new, pristine database
     is returned to the application.
     '''
-    mem2 = create_engine('sqlite://:memory:')
+    mem2 = create_engine('sqlite:///:memory:')
     clone(engine, mem2)
     return mem2, sessionmaker(mem2)
 
 
-class Base(DBase):
-	# This is a base class that we will be using for the Database.
-	# I am expanding upon the original declarative_base class and
-	# adding in the json function (and potentially others) here in
-	# order to conform to DRY principles ;)
-	def json(self, *fields):
-		'''json *fields
-		Returns a Python Dictionary element with the items specified
-		in the fields list.  If no items are specified, then return
-		the endire dictionary.
-		'''
-		jdict = {}
-		if len(fields) == 0:
-			for field in fields:
-				jdict[field] = self.__dict__[field]
-				# Some code will have to be added here to account for
-				# relationship models, however I will have to run some
-				# tests first.
-		else:
-			jdict = self.__dict__
-		return jdict
-
-
 class TextPickle(PickleType):
-	# This is a simple overloaded pickler type so that we can embed JSON
-	# into the database if needed.  This is mostly used with the Version
-	# table, however I placed it here incase it is needed elsewhere.
-	impl = Text
-
-
-# The Category Association Table.  This table houses the relationships
-# between plugins and categories.
-catassc = Table('catagory_associations', DBase.metadata,
-	Column('plugin_id', Integer, ForeignKey('plugin.id')),
-	Column('category_id', Integer, ForeignKey('category.id'))
-)
-
-
-# The Author Association Table.  This table houses the relationships
-# between authors and plugins.
-authassc = Table('author_association', DBase.metadata,
-	Column('author_id', Integer, ForeignKey('author.id')),
-	Column('plugin_id', Integer, ForeignKey('plugin_id'))
-)
+    # This is a simple overloaded pickler type so that we can embed JSON
+    # into the database if needed.  This is mostly used with the Version
+    # table, however I placed it here incase it is needed elsewhere.
+    impl = Text
 
 
 class Plugin(Base):
-	__tablename__ = 'plugin'
-	id = Column(Integer, auto_ncrement=True, primary_key=True)
-	name = Column(String)
-	plugname = Column(String)
-	status = Column(String)
-	link = Column(Text)
-	description = Column(Text)
-	repo_id = Column(Integer, ForeignKey('repo.id'))
-	repo = relationship('Repo', backref='plugins')
-	versions = relationship('Version', order_by='desc(Version.date)',
-							backref='plugin')
-	categories = relationship('Category', secondary=catassc, backref='plugins')
-	authors = relationship('Author', secondary=authassc, backref='plugins')
+    __tablename__ = 'plugin'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String)
+    plugname = Column(String)
+    stage = Column(String)
+    link = Column(Text)
+    description = Column(Text)
+    repo_id = Column(Integer, ForeignKey('repo.id'))
+    repo = relationship('Repo', backref='plugins')
+    versions = relationship('Version', order_by='desc(Version.date)',
+                            backref='plugin')
+    categories = relationship('Category', secondary=catassc, backref='plugins')
+    authors = relationship('Author', secondary=authassc, backref='plugins')
+
+    def __init__(self, name, repo_id):
+        self.name = name
+        self.repo_id = repo_id
 
 
 class Version(Base):
-	__tablename__ = 'version'
-	id = Column(Integer, auto_increment=True, primary_key=True)
-	version = Column(String)
-	link = Column(String)
-	download = Column(String)
-	date = Column(DateTime)
-	md5 = Column(String)
-	status = Column(String)
-	type = Column(String)
-	plugin_id = Column(Integer, ForeignKey('plugin.id'))
-	permissions = Column(TextPickle(pickler=json))
-	commands = Column(TextPickle(pickler=json))
-	hard_dependencies = Column(TextPickle(pickler=json))
-	soft_dependencies = Column(TextPickle(pickler=json))
+    __tablename__ = 'version'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    version = Column(String)
+    link = Column(String)
+    download = Column(String)
+    date = Column(DateTime)
+    md5 = Column(String)
+    status = Column(String)
+    type = Column(String)
+    filename = Column(String)
+    plugin_id = Column(Integer, ForeignKey('plugin.id'))
+    permissions = Column(TextPickle(pickler=json))
+    commands = Column(TextPickle(pickler=json))
+    hard_dependencies = Column(TextPickle(pickler=json))
+    soft_dependencies = Column(TextPickle(pickler=json))
+
+    def __init__(self, name, plugin_id):
+        self.name = name
+        self.plugin_id = plugin_id
 
 
 class Repo(Base):
-	__tablename__ = 'repo'
-	id = Column(Integer, auto_increment=True, primary_key=True)
-	name = Column(String)
+    __tablename__ = 'repo'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String)
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Category(Base):
-	__tablename__ = 'category'
-	id = Column(Integer, auto_increment=True, primary_key=True)
-	name = Column(String)
+    __tablename__ = 'category'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String)
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Author(Base):
-	__tablename__ = 'author'
-	id = Column(Integer, auto_increment=True, primary_key=True)
-	name = Column(String)
+    __tablename__ = 'author'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String)
+
+    def __init__(self, name):
+        self.name = name
 
 
+class Meta(Base):
+    __tablename__ = 'meta'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    repo_id = Column(Integer, ForeignKey('repo.id'))
+    generation = Column(Integer)
