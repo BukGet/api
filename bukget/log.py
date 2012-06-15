0ff22ee19062904fedc6c262b01a18099379728f
Steven McGrath <smcgrath@tenable.com>
2012-06-15 10:35:40 -0500

    Updated logging to properly work from config file ;)

--- bukget/log.py
+++ bukget/log.py
@@ -1,9 +1,17 @@
 import logging
 from bukget.config import config
 
+_loglevels = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warn': logging.WARNING,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL,
+}
+
 log = logging.getLogger('bukget')
 hdlr = logging.FileHandler(config.get('Settings', 'log_file'))
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
-log.setLevel(config.getint('Settings', 'log_level'))
+log.setLevel(_loglevels[config.get('Settings', 'log_level')])
 log.addHandler(hdlr)
