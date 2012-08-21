999a85691c99993e61861c5e5d41ca54846eebff
Steven McGrath <smcgrath@tenable.com>
2012-08-21 11:19:32 -0500

    Service management module

--- bukget/__init__.py
+++ bukget/__init__.py
@@ -2,6 +2,7 @@ import db
 import parsers
 import log
 import config
-#import api
-#import site
-#import server
+import api
+import legacy
+import site
+import svc
---
+++ bukget/svc.py
@@ -0,0 +1,57 @@
+import multiprocessing
+import bottle
+from bukget.config import config
+import bukget.api
+import bukget.site
+import bukget.db
+import time
+from log import log
+from sqlalchemy import desc
+import bukget.parsers
+
+
+def website():
+	initialize()
+	app = bottle.Bottle()
+	if config.getboolean('Settings', 'enable_api'):
+		app.mount('/api', bukget.legacy.app)
+		app.mount('/api2', bukget.api.app)
+	if config.getboolean('Settings', 'enable_site'):
+		app.mount('/', bukget.site.app)
+	bottle.debug(config.getboolean('Settings', 'debug'))
+	bottle.run(app=app,
+			   port=config.get('Settings', 'port'),
+			   host=config.get('Settings', 'host'),
+			   server=config.get('Settings', 'server'),
+			   reloader=config.getboolean('Settings', 'debug'))
+
+
+def updater():
+	initialize()
+	while True:
+		time.sleep(30)
+		log.debug('Running Update Check')
+		for parser in config.get('Settings', 'parsers').split():
+			log.debug('Checking if %s needs to be updated' % parser)
+			s = bukget.db.Session()
+			meta = s.query(bukget.db.Meta).filter_by(repo=parser)\
+					.order_by(desc(bukget.db.Meta.id)).first()
+			s.close()
+			delay = int(time.time() - meta.timestamp)
+			log.debug('%s update delay is currently %s of %s' % (parser, 
+					  delay, config.get(parser, 'update_delay')))
+			if delay >= config.getint(parser, 'update_delay'):
+				bukget.parsers.parsers[parser].run()
+
+
+def initialize():
+	bukget.db.init(bukget.db.disk)
+	s = bukget.db.Session()
+	for parser in config.get('Settings', 'parsers').split():
+		meta = s.query(bukget.db.Meta).filter_by(repo=parser)\
+				.order_by(desc(bukget.db.Meta.id)).first()
+		if meta == None:
+			newmeta = bukget.db.Meta(parser)
+			newmeta.timestamp = 0
+			s.add(newmeta)
+			s.commit()
