9250d6be91fc76a917ccbee69ebbe27b25064a3a
Steven McGrath <smcgrath@tenable.com>
2012-08-21 11:58:34 -0500

    Moved the lookup path for the config file to /etc

--- bukget/config.py
+++ bukget/config.py
@@ -2,4 +2,5 @@ from ConfigParser import ConfigParser
 import os
 
 config = ConfigParser()
-config.read(os.path.join(os.getcwd(), 'bukget.conf'))
+#config.read(os.path.join(os.getcwd(), 'bukget.conf'))
+config.read(os.path.join('etc', 'bukget.conf'))
