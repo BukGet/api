3809383d9f77fce30431621e373c0f7c3d2efaa4
Steven McGrath <steve@chigeek.com>
2012-12-17 10:12:38 -0600

    Setup a working setup script

--- bukget/api/__init__.py
+++ bukget/api/__init__.py
@@ -1,7 +1,6 @@
-import db
 import common
-#import v1
-#import v2
+import v1
+import v2
 import v3
 import update
 import stats
@@ -11,20 +10,14 @@ from bukget.config import config
 
 def start():
     app = Bottle()
-    #app.mount('/1', v1.app)
-    #app.mount('/2', v2.app)
+    app.mount('/1', v1.app)
+    app.mount('/2', v2.app)
     app.mount('/3', v3.app)
     app.mount('/stats', stats.app)
     app.mount('/update', update.app)
 
     @app.get('/')
     def home(): redirect('/3')
-
-    @app.get('/api')
-    def api1(): redirect('/3')
-
-    @app.get('/api2')
-    def api2(): redirect('/3')
 
     debug(config.getboolean('Settings', 'debug'))
     run(app=app,
