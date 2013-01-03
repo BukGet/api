86c1cbec4d0e6cc184afdc5a6591099a5818f5fc
Steven McGrath <smcgrath@tenable.com>
2013-01-02 18:49:40 -0600

    Converted cursors into lists.

--- api/bukgetapi/stats.py
+++ api/bukgetapi/stats.py
@@ -14,22 +14,22 @@ def set_json_header():
 
 @app.get('/naughty_list')
 def naughty_list():
-    plugins = c.db.plugins.find({'_use_dbo': {'$exists': True}}, 
+    plugins = list(c.db.plugins.find({'_use_dbo': {'$exists': True}}, 
                                 {
                                     '_id': 0,
                                     'slug': 1, 
                                     'plugin_name': 1,
                                     'authors': 1,
-                                })
+                                }))
     return c.jsonify(plugins)
 
 
 @app.get('/todays_trends')
 def todays_trends():
     today = datetime.date.today().strftime('%Y-%m-%d')
-    stats = c.db.stats.find({'counts.%s' % today: {'$exists': True}},
+    stats = list(c.db.stats.find({'counts.%s' % today: {'$exists': True}},
                 {'_id': 0, 'slug': 1, 'server': 1, 'counts.%s' % today: 1})\
-                .sort({'counts.%s' % today: -1}).limit(10)
+                .sort({'counts.%s' % today: -1}).limit(10))
     plugins = c.db.plugins.find({},{'slug': 1, 'versions.version': 1})
     pcount = 0
     vcount = 0
