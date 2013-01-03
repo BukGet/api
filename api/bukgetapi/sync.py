a70b32847f987f5094e818762e8b48200361af25
Steven McGrath <smcgrath@tenable.com>
2013-01-02 18:32:24 -0600

    Removed old sync code.

--- api/bukgetapi/sync.py
+++ api/bukgetapi/sync.py
@@ -9,27 +9,3 @@ app = Bottle()
 def set_headers():
     response.set_header('Content-Type', 'application/json')
     response.set_header('Access-Control-Allow-Origin', '*')
-
-
-@app.post('/login')
-def login():
-    '''
-    '''
-    pass
-    
-
-
-@app.put('/plugin')
-def update_plugin():
-    '''
-    '''
-    pass
-
-
-@app.put('/gen')
-def update_geninfo():
-    '''
-    '''
-    pass
-
-
