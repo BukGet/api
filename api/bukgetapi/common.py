9f94e676919605a9272a2784a2f541117525ee25
Steven McGrath <smcgrath@tenable.com>
2013-01-14 10:02:07 -0600

    Forced the reduce function to return integers

--- api/bukgetapi/common.py
+++ api/bukgetapi/common.py
@@ -33,10 +33,10 @@ function () {
 reduceall = Code('''
 function (key, values) {
     var total = 0;
-    for (var i = 0; i < values.length; i++) {
+    for (int var i = 0; i < values.length; i++) {
         total += values[i];
     }
-    return total;
+    return parseInt(total);
 }
 ''')
 
