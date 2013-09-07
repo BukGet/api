d72e49db4d013d6b91eb958679ca4350a7f0f5da
Christopher Matuszak <admin@twisted-networks.com>
2013-09-07 09:26:56 -0400

    Added API call to check versions of multi plugins
    Added an API call to check for multiple plugin versions.

--- api/bukgetapi/v3.py
+++ api/bukgetapi/v3.py
@@ -70,8 +70,27 @@ def plugin_details(server, slug, version=None):
     if size is not None:
         data['versions'] = data['versions'][:size]
     return c.jsonify(data, callback)
-
-
+    
+ @app.get('/plugins/versioncheck/<server>/<slugs>')
+ @app.get('/plugins/versioncheck/<server>/<slugs>/')
+ def versions_check(server,slugs):
+    '''Versions Check
+    Checks the versions of a comma delimited list of slugs
+    '''
+    data = []
+    callback = bleach.clean(request.query.callback or None)
+    fields = ["slug","plugin_name","versions.version"]
+    slugs = bleach.clean(slugs or '').split(',')
+    if len(slugs) > 0:
+        for slug in slugs:
+        	twixted = c.plugin_details(server, slug, version, fields)
+        	if not twixted: twixted['slug'],twixted['plugin_name'],twixted['versions']['version'] = slug,"Unknown","Plugin does not exist"
+        	chaox['slug'],chaox['plugin_name'],chaox['version'] = twixted['slug'],twixted['plugin_name'],twixted['versions']['version']
+        	data.append(chaox)
+        return c.jsonify(data, callback)
+     else:
+        raise HTTPError(400, '{"error": "invalid post"}')
+        	
 @app.get('/plugins/<server>/<slug>/<version>/download')
 def plugin_download(server, slug, version):
     '''Plugin Download Redirector
