f82d1ba137d5c7f99a5b751259d5f80f00c99b95
Steven McGrath <smcgrath@tenable.com>
2014-02-13 18:29:08 -0600

    Removed old API code & removed bleach

--- bukgetapi/__init__.py
+++ bukgetapi/__init__.py
@@ -1,37 +1,17 @@
 import common
-import v1
-import v2
-import v3
+import api
 import sync
 import stats
-from bottle import Bottle, run, debug, redirect, request
-
+from bottle import Bottle, run, debug, redirect, request, ServerAdapter
 
 def start():
     app = Bottle()
-    app.mount('/1', v1.app)
-    app.mount('/2', v2.app)
-    app.mount('/3', v3.app)
+    app.mount('/3', api.app)
     app.mount('/stats', stats.app)
     app.mount('/sync', sync.app)
 
-    # Mounted the applications for v1 and v2 on the legacy URLs as well.  It
-    # appears that some apps arent handling 302 redirects all that well.
-    #app.mount('/api', v1.app)
-    #app.mount('/api2', v2.app)
-
     @app.get('/')
     def home(): redirect('/3')
-
-    @app.get('/api')
-    @app.get('/api/')
-    @app.get('/api/<path:re:(.*)>')
-    def api1(path=''): redirect('/1/%s?%s' % (path, request.query_string))
-
-    @app.get('/api2')
-    @app.get('/api2/')
-    @app.get('/api2/<path:re:(.*)>')
-    def api2(path=''): redirect('/2/%s?%s' % (path, request.query_string))
 
     debug(common.config.getboolean('Settings', 'debug'))
     run(app=app,
