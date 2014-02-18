08a537c4a3bb1252e18c801766f40fa266034942
David Marby <david@dmarby.se>
2014-02-18 18:56:23 +0100

    Leave one core free for MongoDB

--- index.js
+++ index.js
@@ -3,24 +3,18 @@ var cluster = require('cluster');
 
 // Code to run if we're in the master process
 if (cluster.isMaster) {
-
-    // Count the machine's CPUs
-    var cpuCount = require('os').cpus().length;
-
-    // Create a worker for each CPU
-    for (var i = 0; i < cpuCount; i += 1) {
+    // Count the machine's CPU cores
+    var cpuCount = require('os').cpus().length - 1;
+    // Create a worker for each cpu core - 1
+    for (var i = 0, il=cpuCount; i < il; i++) {
         cluster.fork();
     }
-
     // Listen for dying workers
     cluster.on('exit', function (worker) {
-
         // Replace the dead worker, we're not sentimental
-        console.log('Worker ' + worker.id + ' died :(');
+        console.log('Worker ' + worker.id + ' died');
         cluster.fork();
-
     });
-
 // Code to run if we're in a worker process
 } else {
     //Imports
