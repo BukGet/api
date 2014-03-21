c573e955d35c32896f58f693c0c170a8de74ffc3
David Marby <david@dmarby.se>
2014-03-21 18:17:09 +0100

    Change default port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = exports = {
-  port: 9132, // Port that the webserver will run on
+  port: 9130, // Port that the webserver will run on
   address: '127.0.0.1', // Host that the webserver will bind to
 
   database: {
